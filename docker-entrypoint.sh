b4df33fcf9663b77188d0f31610fdfd475260173
Saggi Mizrahi <saggi@mizrahi.cc>
2018-04-18 17:53:11 +0300

    don't run as root

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,5 @@ DATADIR="/config"
 # Start OpenVPN.
 echo "Starting OpenVPN..."
 cd "$DATADIR"
-chown * openvpn:openvn
+chown openvpn:openvn *
 exec openvpn --cipher AES-256-GCM --ncp-ciphers AES-256-GCM:AES-128-GCM:AES-256-CBC:AES-128-CBC:BF-CBC --config server.conf --user openvpn --group openvpn
